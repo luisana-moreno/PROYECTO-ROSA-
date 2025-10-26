efb827bd2d4adf0e21cea31ee55d57287eb6ec8c
Bit9-A <adrian24vergel@gmail.com>
2025-10-25 20:52:05 -0400

    theme

--- src/App.js
+++ src/App.js
@@ -18,21 +18,10 @@ const Page404 = React.lazy(() => import('./views/pages/page404/Page404'))
 const Page500 = React.lazy(() => import('./views/pages/page500/Page500'))
 
 const App = () => {
-  const { isColorModeSet, setColorMode } = useColorModes('coreui-free-react-admin-template-theme')
-  const storedTheme = useSelector((state) => state.theme)
+  const { setColorMode } = useColorModes('light')
 
   useEffect(() => {
-    const urlParams = new URLSearchParams(window.location.href.split('?')[1])
-    const theme = urlParams.get('theme') && urlParams.get('theme').match(/^[A-Za-z0-9\s]+/)[0]
-    if (theme) {
-      setColorMode(theme)
-    }
-
-    if (isColorModeSet()) {
-      return
-    }
-
-    setColorMode(storedTheme)
+    setColorMode('light')
   }, []) // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
